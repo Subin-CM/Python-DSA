ddfe4a9d14e1d31331ed88ef54d2577c30c39345
smohan <smohan@fintech.net>
2025-03-04 14:41:49 +0530

    added codes for Queue

---
+++ Stack/Queue-Enqueue.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def print_queue(self):
+        temp = self.first
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+            
+    def enqueue(self, value):
+        new_node=Node(value)
+        if self.length==0:
+            self.first = new_node
+            self.last = new_node
+        else:
+            temp=self.last
+            temp.next=new_node
+            self.last=new_node
+        self.length+=1 
+
+
+my_queue = Queue(1)
+
+print('Queue before enqueue(2):')
+my_queue.print_queue()
+
+my_queue.enqueue(2)
+
+print('\nQueue after enqueue(2):')
+my_queue.print_queue()
+
+
